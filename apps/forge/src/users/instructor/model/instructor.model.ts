932f7b64488815c6b388f659b0f8b8ef54e3e291
chatwick <c.wick400@gmail.com>
2024-05-09 00:44:00 +0530

    feat: user basic routes

--- apps/forge/src/users/instructor/model/instructor.model.ts
+++ apps/forge/src/users/instructor/model/instructor.model.ts
@@ -1,11 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
-import { USERROLES } from '../constants/user.constants';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { USERROLES } from '../../constants/user.constants';
 
-export type UserDocument = HydratedDocument<User>;
+export type UserDocument = HydratedDocument<Instructor>;
 
 @Schema()
-export class User {
+export class Instructor {
   @Prop({ required: true, minlength: 3 })
   firstName: string;
 
@@ -26,6 +26,13 @@ export class User {
 
   @Prop({ required: true, enum: Object.values(USERROLES) })
   role: string;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'courses',
+    required: true,
+  })
+  publishedCourses: mongoose.Types.ObjectId[];
 }
 
-export const UserSchema = SchemaFactory.createForClass(User);
+export const InstructorSchema = SchemaFactory.createForClass(Instructor);
